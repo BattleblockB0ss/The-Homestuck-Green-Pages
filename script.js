d73d230a13b52aef6cd440c64e2d939039d21dcb
Oskar Brady <57539992+Eaglefeather1@users.noreply.github.com>
2023-03-30 05:07:30 +0000

    made it even better

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ fetch('worldstuck.json')
 		return response.json();
 	})
 	.then(function(data) {
-		var numRegions = Object.values(data).length
-		for (var a = 0; a < numRegions; a++) {
+		var regions = Object.values(data);	
+		for (var a = 0; a < regions.length; a++) {
+			regions[a].sort(function(a, b) {
+				return a.groupName.localeCompare(b.groupName);
+			});
 			appendData(data, a);
 		}
 	})
@@ -12,30 +15,44 @@ fetch('worldstuck.json')
 		console.log('error: ' + err);
 	});
 function appendData(data, a) {
+
+	//VARIABLES
 	var mainContainer = document.getElementById("myData");
-	var regionGroups = Object.values(data)[a]
-	for (var i = 0; i < regionGroups.length; i++) {
-		var group = regionGroups[i];
-		function appendDiv() {
-			var div = document.createElement("div");
-			div.setAttribute("class", `groupSection group-${group.id}`);
-			mainContainer.appendChild(div);
+	var regionKeys = Object.keys(data)[a];
+	var regionValues = Object.values(data)[a];
+	var regions = Object.values(data)
+	//END VARIABLES
+
+	//FUNCTIONS
+	function appendDiv(classes, idNeed, divId, container) {
+		let div = document.createElement("div");
+		div.setAttribute("class", classes);
+		if (idNeed) {
+			div.setAttribute("id", divId);
 		}
-		function appendImg() {
-			var img = new Image();
-			img.src = group.img;
-			img.setAttribute("class", "image");
-			img.setAttribute("alt", "MSPA Reader");
-			mainContainer.lastChild.appendChild(img);
-		}
-		function appendPar() {
-			var par = document.createElement("p");
-			par.innerHTML = `<b>Name: </b>${group.groupName}`;
-			par.setAttribute("class", "para");
-			mainContainer.lastChild.appendChild(par);
-		}
-		appendDiv();
-		appendPar();
-		appendImg();
+		container.appendChild(div);
 	}
+	function appendImg(alt, container) {
+		let img = new Image();
+		img.src = regionValues[i].img;
+		img.setAttribute("alt", alt);
+		container.appendChild(img);
+	}
+	function appendText(textVal, container, textTag) {
+		let text = document.createElement(textTag);
+		text.innerHTML = textVal;
+		container.appendChild(text);
+	}
+	//END FUNCTIONS
+
+	//THE GOOD SHIT
+	appendDiv("region", true, regionKeys, mainContainer);
+	appendText(Object.keys(data)[a], mainContainer.lastChild, "h1")
+	for (var i = 0; i < regionValues.length; i++) {
+		appendDiv("group", false, "", mainContainer.lastChild);
+		groupDiv = mainContainer.lastChild.lastChild
+		appendText(`<b>Name: </b>${regionValues[i].groupName}`, groupDiv, "p");
+		appendImg("wowza", groupDiv);
+	}
+	//END THE GOOD SHIT
 }
